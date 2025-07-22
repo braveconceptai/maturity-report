b999ec978c8eda7c8173fb39be901c089049efbe
braveconceptai <tony@braveconcept.ai>
2025-07-22 13:45:36 -0500

    Update mailgunClient.js

--- mailgunClient.js
+++ mailgunClient.js
@@ -1,12 +1,10 @@
-// mailgunClient.js
 const formData = require('form-data');
-// note the .default here:
-const Mailgun  = require('mailgun.js').default;
+const Mailgun = require('mailgun.js').default;
 
-// initialize the v4 client
+// Initialize the v4 client
 const mg = new Mailgun(formData).client({
-  username: 'api',                    // always "api" for Mailgun
-  key:      process.env.MAILGUN_API_KEY
+  username: 'api',
+  key: process.env.MAILGUN_API_KEY
 });
 
 module.exports = mg;
